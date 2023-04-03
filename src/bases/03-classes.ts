70f5352eab5fc823f47427c738a2bd267fc8da4d
sebasGZA <sebassga34@gmail.com>
2023-04-03 11:17:22 -0500

    typing http response

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,4 +1,8 @@
 import axios from "axios";
+import {
+  Move,
+  PokeAPIResponse,
+} from "../interfaces/pokeapi-response.interface";
 // export class Pokemon {
 //   public id: number;
 //   public name: string;
@@ -26,8 +30,10 @@ export class Pokemon {
     console.log(`${this.name}, ${this.name}`);
   }
 
-  async getMoves() {
-    const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon/4");
+  async getMoves(): Promise<Move[]> {
+    const { data } = await axios.get<PokeAPIResponse>(
+      "https://pokeapi.co/api/v2/pokemon/4"
+    );
     console.log(data.moves);
     return data.moves;
   }
